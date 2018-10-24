3e72766378c8ff37c8ea9324b1248739cfb3f883
Mithreindeir <mithreindeir@gmail.com>
2018-10-24 15:30:37 -0400

    Update ast.h

--- src/ast.h
+++ src/ast.h
@@ -78,6 +78,7 @@ struct func {
 	struct stmt *body;
 };
 
+/*Compound statement, contains symbol table of scope*/
 struct block {
 	struct stmt **stmt_list;
 	int num_stmt;
@@ -85,110 +86,30 @@ struct block {
 	struct symbol_table *scope;
 };
 
+/*Iterative statements, for, while, do while*/
 struct loop {
 	struct expr *init, *condition, *iter;
 	struct stmt *body;
 };
 
+/*Conditional statement, if, if/else, switch*/
 struct cond {
 	struct expr *condition;
 	struct stmt *body;
 	struct stmt *otherwise;
 };
 
+/*Binary operation*/
 struct binop {
 	struct expr *lhs, *rhs;
 	enum operator op;
 };
 
+/*Unary operation*/
 struct unop {
 	struct expr *term;
 	enum operator op;
 };
-
-/*Declarations are a bitch:
- * The grammar is as follows:
- * declaration:
- *  		| { declaration-specifier }+ { init-declarator }*
- *
- * init-declarator:
- * 		| declarator
- * 		| declarator = intializer
- * 		;
- *
- * declaration-specifier:
- *  		| storage-class-specifier
- *  		| type-specifier
- *  		| type-qualifier
- *  		;
- *
- * declarator:
- * 		| {ptr}? dir_declr
- * 		;
- * direct declarator:
- * 		| ident
- *  		| ( declarator )
- *  		| direct_declarator [ const expr ]
- *  		| direct_declarator ( param list )
- *  		| direct_declarator ( {ident}* )
- *  		;
- *
- * Hence:
- * int; //legal
- * int int; //legal parsing technically, but error (two or more data types in decl-spec) so just throw error
- * const a; //legal, warning about the int default
- * //both different
- * int (*ptr)[2];
- * int   *ptr[2];
- *
- * //Functions make it worse
- * int (*func)(); //not too bad
- * int **(*func)(struct { int a; int b; }, const char *p[10]); //completely legal
- * basically, parentheses redirect the declarator to inject a pointer into any type
- * int *func(); //function declaration with return type of int*
- * int (*func)(); //function pointer declaration with return type of int
- * int * ->  func  -> empty-parameter-list;
- * int   -> *func  -> empty-parameter-list;
- *
- *
- * declaration's have a type name a list of 0 or more type qualifiers and they have an arbitrary
- * amount of indirection, array specifiers, function specifiers, identifiers, etc
- * additionally, any functions introduce this concept recursively, as well as possibly adding
- * "abstract" versions
- *
- * In practice, 90% of times this can be squashed into a struct like:
- * declaration {
- *  	type;
- * 	num_ptr;
- * 	identifier;
- * };
- * And adding recursion, as well as array specifiers allow all types to be expressed
- * declaration {
- * 	type;
- * 	num_ptr;
- * 	identifier;
- * 	array_constexpr;
- * 	*next;
- * };
- *
- * */
-
-
-/*The goal is to have it possible for simple types to fit in a single struct:
- * int *a; 			//fits easily into below struct
- * int *(*a)(int [][][]b); 	//it works with like 7 layers of indirection, but why complain
- * This list should be the canonicalization of:
- * int *a; = ptr(int) a;
- * int *(*a)(int [][][]b); = ptr(int) -> ptr(func(arr(arr(arr(int))) -> b)) -> a;
- * */
-/*A declaration is formed by nesting each specifier
- * int  *(*a)[2]; //pointer to array of 2 pointers to integers
- * "a -> ptr -> arr[2] -> ptr -> int"
- * This form is good, because the only operation needed to dereference/index is removing the first
- * element after the identifier, which is quant because it becomes an expression without an ident
- * *a; //arr[2]
- * arr[2] -> ptr -> int;
- * */
 
 struct declaration {
 	char *ident;
@@ -197,6 +118,7 @@ struct declaration {
 	struct expr *initializer;
 };
 
+/*Primary expression or unit value in expression*/
 union value {
 	char *ident;
 	long cnum;
